bd0b5aa7c3a22b0bf46c46b4d0f23052b0d385d6
None <prernacodes@gmail.com>
2022-10-13 12:54:48 +0530

    Js Variables

---
+++ Introduction of JS (Variables)/problem2.js
@@ -0,0 +1,16 @@
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let name= "Prerna";      // My name
+console.log(name);
+name = "Mahendra Sah";   //Father's name
+console.log(name);
+name ="Vineeta Sah"      //Mother's name
+console.log(name )
+
+ 
+
